79773e650da959cbc9d1c79ea147db1163a4779d
Jun Ren <chanjunren@gmail.com>
2023-04-02 23:08:40 +0800

    :adhesive_bandage: Minor fix

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,24 +1,24 @@
 -- Learn through :help options
 
 local o = vim.opt
- 
+
 o.backup = false
-o.clipboard = "unnamedplus" 
-o.cmdheight = 2 
+o.clipboard = "unnamedplus"
+o.cmdheight = 2
 o.completeopt = { "menuone", "noselect" }
 o.fileencoding = "utf-8"
 o.hlsearch = true
 o.ignorecase = true
 o.pumheight = 10
 o.showtabline = 2
-o.cursorline = false 
+o.cursorline = false
 o.writebackup = false
 o.number = true
 o.relativenumber = true
 o.numberwidth = 2
 o.guifont = "monospace:h17"
 o.termguicolors = true
-o.syntax = enable
+o.syntax = true
 o.expandtab = false
 o.showtabline = 2
 o.shiftwidth = 2
