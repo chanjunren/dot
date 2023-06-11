8a8ede9047f4566e872abad71fffa46301388083
junren.chan <junren.chan@okg.com>
2023-06-11 10:35:59 +0800

    :sparkles: Updated .ideavimrc and pushed .brew-packages.txt

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,17 @@
 if status is-interactive
-	# Commands to run in interactive sessions can go here
-	nvm use v18.15.0 --silent
-    
-	# Init oh-my-posh theme
-	eval "$(oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/takuya.omp.json)"
-	
-	# Aliases
-	if [ -f $HOME/.config/fish/alias.fish ]
-		source $HOME/.config/fish/alias.fish
-	end
-    
+    # Commands to run in interactive sessions can go here
+    nvm use v18.15.0 --silent
+
+    # Init oh-my-posh theme
+    eval "$(oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/easy-term.omp.json)"
+
+    # Aliases
+    if [ -f $HOME/.config/fish/alias.fish ]
+        source $HOME/.config/fish/alias.fish
+    end
+
     # Path Variables
-   	if [ -f $HOME/.config/fish/paths.fish ]
+    if [ -f $HOME/.config/fish/paths.fish ]
         #source $HOME/.config/fish/paths.fish
     end
 end
