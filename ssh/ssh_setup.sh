84640d3535355fa34e0b90256e9d685b6259ddcd
chanjunren <chanjunren@gmail.com>
2023-10-10 15:47:13 +0800

    :sparkles: Added comment

--- ssh/ssh_setup.sh
+++ ssh/ssh_setup.sh
@@ -6,5 +6,6 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519_github
 ssh-add ~/.ssh/id_ed25519_gitlab
 
+# Copy to paste and add to the relevant account
 # pbcopy < ~/.ssh/id_ed25519_github.pub
 # pbcopy < ~/.ssh/id_ed25519_gitlab.pub
