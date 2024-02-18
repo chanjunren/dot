a70b53c3ca19975149a696433f4443ee74109020
junren.chan <junren.chan@okg.com>
2024-02-18 11:08:24 +0800

    :sparkles: Changed nvim theme

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -1,13 +1,12 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "latte", -- latte, frappe, macchiato, mocha
-    },
-  },
-  -- Configure LazyVim to load gruvbox
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd("colorscheme github_light_default")
+    end,
+  }, -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
