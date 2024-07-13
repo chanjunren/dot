7d2912e23f0359e91b98d2ec852a9aeae14d09bd
junren.chan <junren.chan@okg.com>
2024-07-13 22:46:55 +0800

    :lipstick: Add githubDark

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -2,12 +2,13 @@ return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
   { "wittyjudge/gruvbox-material" },
+  {'projekt0n/github-nvim-theme'},
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "github_dark_tritanopia",
     },
   },
 }
