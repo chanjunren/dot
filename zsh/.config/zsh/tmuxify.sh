1d17f75e70f50305321983ed59170b8164bd0f70
junren.chan <junren.chan@okg.com>
2024-10-21 10:36:55 +0800

    :technologist: Refactored tmuxify.sh, added aerospace config

--- zsh/.config/zsh/tmuxify.sh
+++ zsh/.config/zsh/tmuxify.sh
@@ -1,5 +1,22 @@
+inTmuxSession() {
+  [[ -n "$TMUX" ]]
+}
 
-if [[ -z "$TMUX" ]]; then
+inVscodeIDE() {
+  [[ "$TERM_PROGRAM" == "vscode" || -n "$VSCODE_INJECTION" ]]
+}
+
+inIntellijIDE() {
+  [[ -n "$TERMINAL_EMULATOR" ]]
+}
+
+inIntegratedIDE() {
+  inVscodeIDE || inIntellijIDE
+}
+
+
+
+if ! inTmuxSession && ! inIntegratedIDE; then
   # Check if there is any existing tmux session
   if tmux ls >/dev/null 2>&1; then
     # Attach to the first session found
