a6b31d23f600dc574d9b34e8c1ff195cd2d9cd9e
junren.chan <junren.chan@okg.com>
2025-01-29 10:31:04 +0800

    ✨ Update wsl path

--- .home/.config/zsh/paths_wsl.sh
+++ .home/.config/zsh/paths_wsl.sh
@@ -2,9 +2,12 @@ export BREW_HOME="/home/linuxbrew/.linuxbrew/bin"
 PATH=$BREW_HOME:$PATH
 
 NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 PATH=$NVM_DIR:$PATH
+
+export JAVA_HOME=/usr/local/java-sdk/
+PATH="$JAVA_HOME/bin:$PATH"
 
 PATH="/home/linuxbrew/.linuxbrew/sbin:$PATH"
 
