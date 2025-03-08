9a17401157b5d113786ba635694b790742b46d6f
junren.chan <junren.chan@okg.com>
2025-03-09 00:36:28 +0800

    ✨ Rose pine still best theme

--- .home/.config/brew/init.sh
+++ .home/.config/brew/init.sh
@@ -6,7 +6,7 @@ NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/powerlevel10k_rainbow.omp.json)"
-eval "$(oh-my-posh init zsh --config ${XDG_CONFIG_HOME}/brew/poshThemes/robbyrussell.omp.json)"
+eval "$(oh-my-posh init zsh --config ${XDG_CONFIG_HOME}/brew/posh_themes/rose-pine.omp.json)"
 # eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/gruvbox.omp.json)"
 # eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/brew/github_dark_tritanopia.omp.json)"
 # eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/brew/custom1.omp.json)"
